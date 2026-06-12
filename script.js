323efe383d9dde10618cf96f7ddf94b8186c43ed
Yash solanki <ys1084711@gmail.com>
2026-06-12 23:13:11 +0530

    Netflix

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hey i am a conditional tutorial")
+
+let age = 5;
+let grace = 5;
+
+console.log(grace - age)
+console.log(grace * age)
+console.log(grace + age)
+console.log(grace / age)
+console.log(grace ** age)
+
+if((grace - age)>18){
+    console.log("you can drive")
+} 
